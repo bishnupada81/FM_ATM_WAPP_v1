0b71c24fbf343bac8ad884781aa606a807c87666
bishnupada81 <ayerish9596@yahoo.com>
2024-03-07 20:35:32 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
+import { MobileNumberComponent } from './component/mobile-number/mobile-number.component';
+import { EnterPinComponent } from './component/enter-pin/enter-pin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',                    component:LandingPageComponent},
+  {path:'enter-mobile',        component:MobileNumberComponent},
+  {path:'enter-pin',           component:EnterPinComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
+import { MobileNumberComponent } from './component/mobile-number/mobile-number.component';
+import { EnterOtpComponent } from './component/enter-otp/enter-otp.component';
+import { EnterPinComponent } from './component/enter-pin/enter-pin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    MobileNumberComponent,
+    EnterOtpComponent,
+    EnterPinComponent
   ],
   imports: [
     BrowserModule,
