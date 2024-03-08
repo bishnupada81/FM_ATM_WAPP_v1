b562d199b25d6699d9ee1af850e3b9bcbc51839a
bishnupada81 <ayerish9596@yahoo.com>
2024-03-08 11:41:10 +0530

    change route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './component/landing-page/landing-page.component';
 import { MobileNumberComponent } from './component/mobile-number/mobile-number.component';
 import { EnterPinComponent } from './component/enter-pin/enter-pin.component';
+import { EnterOtpComponent } from './component/enter-otp/enter-otp.component';
 
 const routes: Routes = [
   {path:'',                    component:LandingPageComponent},
   {path:'enter-mobile',        component:MobileNumberComponent},
-  {path:'enter-pin',           component:EnterPinComponent},
+  {path:'enter-otp',           component:EnterOtpComponent},
 ];
 
 @NgModule({
